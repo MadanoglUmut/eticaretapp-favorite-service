ca5c162f508b085d9392412bf776ba0eeded9678
Umut <umutreis47@gmail.com>
2025-03-22 12:09:39 +0300

    updated list repo

--- internal/repositories/favorite_list_repository.go
+++ internal/repositories/favorite_list_repository.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"context"
 	"favorite_service/internal/models"
 
 	"gorm.io/gorm"
@@ -16,10 +17,10 @@ func NewFavoriteListRepository(db *gorm.DB) *FavoriteListRepository {
 	}
 }
 
-func (r *FavoriteListRepository) GetFavoriteList(userId int) ([]models.FavoriteList, error) {
+func (r *FavoriteListRepository) GetFavoriteList(ctx context.Context, userId int) ([]models.FavoriteList, error) {
 	var favoriteList []models.FavoriteList
 
-	if err := r.db.Table("favoritelist").Debug().Where("userid = ?", userId).Find(&favoriteList).Error; err != nil {
+	if err := r.db.WithContext(ctx).Table("favoritelist").Debug().Where("userid = ?", userId).Find(&favoriteList).Error; err != nil {
 		return nil, err
 	}
 
@@ -27,25 +28,25 @@ func (r *FavoriteListRepository) GetFavoriteList(userId int) ([]models.FavoriteL
 
 }
 
-func (r *FavoriteListRepository) CreateFavoriteList(favoriteList *models.FavoriteList) error {
+func (r *FavoriteListRepository) CreateFavoriteList(ctx context.Context, favoriteList *models.FavoriteList) error {
 
-	if err := r.db.Debug().Table("favoritelist").Create(&favoriteList).Error; err != nil {
+	if err := r.db.WithContext(ctx).Debug().Table("favoritelist").Create(&favoriteList).Error; err != nil {
 		return err
 
 	}
 	return nil
 }
 
-func (r *FavoriteListRepository) UpdateFavoriteList(listId int, updtList models.UpdateFavoriteList) (models.FavoriteList, error) {
+func (r *FavoriteListRepository) UpdateFavoriteList(ctx context.Context, listId int, updtList models.UpdateFavoriteList) (models.FavoriteList, error) {
 
 	var favoriList models.FavoriteList
-	if err := r.db.Table("favoritelist").First(&favoriList, listId).Error; err != nil {
+	if err := r.db.WithContext(ctx).Table("favoritelist").First(&favoriList, listId).Error; err != nil {
 		return models.FavoriteList{}, err
 	}
 
 	favoriList.ListName = updtList.ListName
 
-	if err := r.db.Table("favoritelist").Save(&favoriList).Error; err != nil {
+	if err := r.db.WithContext(ctx).Table("favoritelist").Save(&favoriList).Error; err != nil {
 		return models.FavoriteList{}, err
 	}
 
@@ -53,9 +54,9 @@ func (r *FavoriteListRepository) UpdateFavoriteList(listId int, updtList models.
 
 }
 
-func (r *FavoriteListRepository) DeleteFavoriteList(listId int) error {
+func (r *FavoriteListRepository) DeleteFavoriteList(ctx context.Context, listId int) error {
 
-	result := r.db.Table("favoritelist").Where("id = ?", listId).Delete(&models.FavoriteList{})
+	result := r.db.WithContext(ctx).Table("favoritelist").Where("id = ?", listId).Delete(&models.FavoriteList{})
 
 	if result.Error != nil {
 		return result.Error
@@ -69,18 +70,16 @@ func (r *FavoriteListRepository) DeleteFavoriteList(listId int) error {
 
 }
 
-//BU FONKSİYON TEK DEĞER DÖNMESİN
-
-func (r *FavoriteListRepository) GetListOwner(listId int) (int, error) {
+func (r *FavoriteListRepository) GetListOwner(ctx context.Context, listId int) (models.FavoriteList, error) {
 
 	var favoriteList models.FavoriteList
 
-	if err := r.db.Table("favoritelist").Where("id = ?", listId).First(&favoriteList).Error; err != nil {
+	if err := r.db.WithContext(ctx).Table("favoritelist").Where("id = ?", listId).First(&favoriteList).Error; err != nil {
 
-		return 0, err
+		return models.FavoriteList{}, err
 
 	}
 
-	return favoriteList.UserId, nil
+	return favoriteList, nil
 
 }
